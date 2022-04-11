ab383f0a2a33b8f485b21d05ee929bf546b9a15b
Paul Smart <pdsmart6@gmail.com>
2022-04-11 16:36:44 +0100

    requirements complete

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
@@ -14,11 +15,17 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
-
+    if(req.body.username !== mockUser.username) res.json({error: 'invalid username'})
+    if (req.body.password !== mockUser.password)  res.json({error: 'invalid password'})
+    const token = jwt.sign(mockUser, 'key')
+    res.json({jwt: token})
 });
 
 router.get('/profile', (req, res) => {
-
+    let profile
+   try {profile = jwt.verify(req.headers.authorization,'key')}
+   catch {return "error"}
+   res.json({profile: profile.profile})
 });
 
 module.exports = router;
